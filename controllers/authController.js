14287b966c4bac4b86d200d63292e631161895c3
Oponjousjoe <joe.ikdura@gmail.com>
2025-01-28 07:38:17 +0100

    Send jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,13 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expiresIn: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE_IN * 24 * 60 * 60 * 1000
+    ),
+  };
+  if (process.env.NODE_ENV === "production") httpOnly: true;
+  res.cookie("jwt", token, cookieOptions);
 
   res.status(statusCode).json({
     status: "success",
