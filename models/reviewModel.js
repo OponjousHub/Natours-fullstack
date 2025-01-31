06a3b670fd230080ed7ba204fa7842122bb0e3fc
Oponjousjoe <joe.ikdura@gmail.com>
2025-01-31 14:35:39 +0100

    Added feature to calculate average Rating and average Quantity

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
     review: {
       type: String,
       required: [true, "Please enter review texts!"],
-      unique: true,
+      // unique: true,
     },
     rating: {
       type: Number,
@@ -42,6 +43,43 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+// The function that calculate the avgRating and avgQuantity
+reviewSchema.statics.calcAgerageRating = async function (tourId) {
+  console.log(tourId);
+  const stats = await Review.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  console.log(stats);
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingAverage: stats[0].avgRating,
+    ratingQuantity: stats[0].nRating,
+  });
+};
+
+// Calling the abaove function to calculate ratings when creating a review
+reviewSchema.post("save", function () {
+  this.constructor.calcAgerageRating(this.tour);
+});
+
+// Calling the abaove function to calculate ratings when updaataing or deleting a revi
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAgerageRating(this.r.tour);
 });
 
 const Review = mongoose.model("Review", reviewSchema);
